51504c5f509aed80e5043bd79581f59f2d6896c1
Paulo Felipe <PauloFelipet15@gmail.com>
2022-11-25 19:35:56 -0300

    First Commit app task

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  Container: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#16B9DD",
+  },
+
+  main: {
+    display: "flex",
+    width: "100%",
+    height: 583,
+    alignItems: "center",
+  },
+
+  
+});
+
+export { styles };
