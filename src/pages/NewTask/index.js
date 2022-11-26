1a28e5c65123327594a5172ab8934f6cbc700e4e
Paulo Felipe <PauloFelipet15@gmail.com>
2022-11-26 19:38:51 -0300

    commit

--- src/pages/NewTask/index.js
+++ src/pages/NewTask/index.js
@@ -1,16 +1,36 @@
 import React from "react";
-import { Text, View, StatusBar, Input } from "react-native";
+import { Text, View, StatusBar, Input, TextInput } from "react-native";
 import { styles } from "./styles";
 import TitleApp from "../../components/TitleApp";
- 
+import { useState } from "react";
 
 export default function NewTask() {
+  const [title, seTitle] = useState("");
+  const [task, seTask] = useState("");
+  const [date, seDate] = useState(Date.now());
   return (
     <View style={styles.Container}>
       <StatusBar />
-      <TitleApp title="Criar Tarefa"/>
+      <TitleApp title="CRIAR TAREFA" />
+
       <View style={styles.main}>
-         
+        <View style={styles.taskContainer}>
+          <View style={styles.labelTitle}>
+            <Text style={styles.title}>Escolha um Titulo:</Text>
+            <TextInput
+              style={styles.inputTitle}
+              placeholder="EX: Jantar com os amigos"
+            ></TextInput>
+          </View>
+
+          <View style={styles.labelTask}>
+            <Text style={styles.title}>Faça sua Anotação:</Text>
+            <TextInput
+              style={styles.inputTask}
+              placeholder="Digite Aqui sua anotação..."
+            ></TextInput>
+          </View>
+        </View>
       </View>
     </View>
   );
--- src/pages/NewTask/styles.js
+++ src/pages/NewTask/styles.js
@@ -11,7 +11,50 @@ const styles = StyleSheet.create({
     display: "flex",
     width: "100%",
     height: 583,
+  },
+
+  taskContainer: {
+    width: "100%",
+    height: 400,
+    paddingHorizontal: 15,
+  },
+
+  labelTitle: {
+    marginTop: 40,
+    marginBottom: 40,
+    width: "100%",
+    maxWidth: "80%",
+    height: 80,
+  },
+  title: {
+    fontSize: 25,
+  },
+  inputTitle: {
+    width: 340,
+    height: 35,
+    fontSize: 20,
+    backgroundColor: "rgba(255,2555,255,0.8)",
     alignItems: "center",
+    paddingLeft: 8,
+    borderRadius: 15,
+  },
+
+  labelTask: {
+    width: "100%",
+    maxWidth: "99%",
+    height: 80,
+  },
+
+  inputTask: {
+    width: "100%",
+    height: 120,
+    fontSize: 20,
+    backgroundColor: "rgba(255,2555,255,0.8)",
+    alignItems: "center",
+
+    borderRadius: 15,
+    paddingLeft: 8,
   },
 });
+
 export { styles };
